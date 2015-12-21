95b64b380f1173eaa9026838667345d8078cd5c2
Jake Mor <jakemny@gmail.com>
2015-12-21 17:38:05 -0500

    finished update sources endpoint

--- app/App.php
+++ app/App.php
@@ -12,7 +12,8 @@ ssh -N -L 8888:127.0.0.1:80 -i key.pem bitnami@IP
 
 // MARK: App
 require_once __DIR__ . '/Engine.php';
-
+require_once __DIR__ . '/User.php';
+require_once __DIR__ . '/Source.php';
 
 class App extends Engine {
 	/* 
@@ -21,9 +22,83 @@ class App extends Engine {
 
 	*/
 
-	public function test($get, $post) {
+	// import panda sources
+	public function updateSources($get, $post) {
+		$sources = file_get_contents("http://api.pnd.gs/v1/sources/");
+		$sources = json_decode($sources, true);
+		
+		$my_sources = []; 
 
-		$this->response->add("get", $get);
+		foreach ($sources as $source) {
+
+			// get source and store it in an array
+			$my_source = [];
+			$my_source["panda_id"] = $source["_id"];
+			$my_source["key"] = $source["key"];
+			$my_source["name"] = $source["name"];
+			$my_source["description"] = $source["description"];
+			$my_source["category"] = $source["category"];
+			$my_source["type"] = $source["type"];
+			$my_source["icon"] = $source["icon"];
+			$my_source["color"] = $source["ios"]["color"];
+			$my_source["popularity"] = $source["stats"]["popularity"]; 
+			$my_source["popular_endpoint"] = NULL;
+			$my_source["latest_endpoint"] = NULL;
+
+			if ($source["popular"]) {
+				$my_source["popular_endpoint"] = $source["endpoints"]["popular"];
+			}
+
+			if ($source["latest"]) {
+				$my_source["latest_endpoint"] = $source["endpoints"]["latest"];
+			}
+
+			$display_settings = $source["settings"]["display"];
+
+			// meta stuff
+			$supported_meta = []; 
+
+			if ($display_settings["views"]) {
+				array_push($supported_meta, "views"); 
+			}
+
+			if ($display_settings["votes"]) {
+				array_push($supported_meta, "votes"); 
+			}
+
+			if ($display_settings["comments"]) {
+				array_push($supported_meta, "comments"); 
+			}
+
+			if ($display_settings["author"]) {
+				array_push($supported_meta, "author"); 
+			}
+
+			$my_source["meta"] = implode(",", $supported_meta);
+
+			$s = new Source($this->ds);
+			$s = $s->withPandaId($my_source["panda_id"]);
+
+			if (is_null($s)) {
+				$s = new Source($this->ds);
+			}
+
+			$s->fromRow($my_source);
+			$s->save();
+			array_push($my_sources, $s);
+		}
+
+		function cmp($a, $b) {
+			if ($a->popularity == $b->popularity) {
+				return 0;
+			}
+
+			return ($a->popularity > $b->popularity) ? -1 : 1;
+		}
+
+		usort($my_sources, "cmp");
+
+		$this->response->add("sources", $my_sources);
 	}
 
 	/* 
